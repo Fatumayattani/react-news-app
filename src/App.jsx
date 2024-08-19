f55337fa79f6943c0c3ed6418348530ffc31be78
Fatumayattani <fyattani@gmail.com>
2024-08-19 12:56:18 +0300

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react"
 import Navbar from "./Components/Navbar"
 import NewBoard from "./Components/NewBoard"
 
 
 const App = () => {
+  const [category,setCategory] = useState("general")
   return (
     <div>
-      <Navbar/>
-      <NewBoard/>
+      <Navbar setCategory={setCategory}/>
+      <NewBoard category={category}/>
 
     </div>
   )
--- src/Components/NewBoard.jsx
+++ src/Components/NewBoard.jsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 
-const NewBoard = () => {
+const NewBoard = ({ category }) => {  // Destructure category prop
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
-        let url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${import.meta.env.VITE_API_KEY}`;
+        let url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
         fetch(url)
             .then(response => response.json())
             .then(data => setArticles(data.articles));
-    }, []);
+    }, [category]);  // Add category to dependency array
 
     return (
         <div>
             <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-            {articles.map((news,index)=>{
-                return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+            {articles.map((news, index) => {
+                return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
             })}
         </div>
     );
